65b8e22332283327305475768e3f18a9787cbb9e
shinjw <jwshin@rectuson.com>
2022-01-24 12:20:55 +0900

    user tweet resource 생성 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TweetsModule } from './tweets/tweets.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TweetsModule],
   controllers: [AppController],
   providers: [AppService],
 })
